8018b0273182ab6e12d2b36c8a0760c8ba8b3ae7
Daniel Prieto Remacha <79273379+dprieto96@users.noreply.github.com>
2023-04-16 19:55:28 +0200

    more buttons on menu

--- src/js/scenes/MenuScene.js
+++ src/js/scenes/MenuScene.js
@@ -19,23 +19,71 @@ export default class MenuScene extends Phaser.Scene {
 
     preload(){
         this.load.image('button', 'assets/img/button.png');
+        this.load.image('icon', 'assets/img/web/CodeChronicles.png');
+        this.load.image('created', 'assets/img/web/Game-created-by.png');
     }
 
     create(){
-        this.text=this.add.text(SCREEN_MAX_WIDTH/2+40,SCREEN_HEIGHT/2, "START",{ fontStyle: 'strong',font: '40px Arial', fill: '#ffffff' });
-        this.text.setDepth(999);
+        this.scale=0.3;
 
-        this.button = this.add.image(SCREEN_MAX_WIDTH/2+100,SCREEN_HEIGHT/2+30,'button');
-        this.button.setDepth(998);
-        this.button.setScale(0.5);
+        
+        
+        //BUTTON START
+        this.buttonSTART = this.add.image(SCREEN_MAX_WIDTH/2+100,200,'button');
+        this.buttonSTART.setDepth(998);
+        this.buttonSTART.setScale(this.scale);
+        this.buttonSTART.setInteractive();
+        this.buttonSTART.on('pointerup', function () {this.scene.start('levelSelector')}, this);
+        this.textSTART=this.add.text(SCREEN_MAX_WIDTH/2+50,185, "START",{ fontStyle: 'strong',font: '30px Arial', fill: '#ffffff' });
+        this.textSTART.setDepth(999);
 
-        this.button.setInteractive();
-        this.button.on('pointerup', function () {this.scene.start('levelSelector')}, this);
+        //BUTTON CONTROL
+        this.buttonCONTROLS = this.add.image(SCREEN_MAX_WIDTH/2+100,300,'button');
+        this.buttonCONTROLS.setDepth(998);
+        this.buttonCONTROLS.setScale(this.scale);
+        this.buttonCONTROLS.setInteractive();
+        //this.buttonCONTROLS.on('pointerup', function () {this.redirectToWeb()}, this);
+        this.textCONTROL=this.add.text(SCREEN_MAX_WIDTH/2+20,285, "CONTROLS",{ fontStyle: 'strong',font: '30px Arial', fill: '#ffffff' });
+        this.textCONTROL.setDepth(999);
+
+        //BUTTON HISTORY
+        this.buttonHISTORY = this.add.image(SCREEN_MAX_WIDTH/2+100,400,'button');
+        this.buttonHISTORY.setDepth(998);
+        this.buttonHISTORY.setScale(this.scale);
+        this.buttonHISTORY.setInteractive();
+        //this.buttonHISTORY.on('pointerup', function () {this.scene.start('levelSelector')}, this);
+        this.textHISTORY=this.add.text(SCREEN_MAX_WIDTH/2+40,385, "HISTORY",{ fontStyle: 'strong',font: '30px Arial', fill: '#ffffff' });
+        this.textHISTORY.setDepth(999);
+
+        //BUTTON GITYCREATORS
+        this.buttonGITYCREATORS = this.add.image(SCREEN_MAX_WIDTH/2+100,500,'button');
+        this.buttonGITYCREATORS.setDepth(998);
+        this.buttonGITYCREATORS.setScale(this.scale);
+        this.buttonGITYCREATORS.setInteractive();
+        this.buttonGITYCREATORS.on('pointerup', function () {this.redirectToWeb()}, this);
+        this.textGITYCREATORS=this.add.text(SCREEN_MAX_WIDTH/2+40,470, "GITHUB &\nCREATORS",{ fontStyle: 'strong',font: '25px Arial', fill: '#ffffff' });
+        this.textGITYCREATORS.setDepth(999);
+
+        //CodeChronicles
+        this.icon = this.add.image(SCREEN_MAX_WIDTH/2+100,SCREEN_MAX_HEIGHT,'icon');
+        this.icon.setDepth(999);
+        this.icon.setScale(this.scale+0.3);
+
+        this.created = this.add.image(SCREEN_MAX_WIDTH/2+100,SCREEN_MAX_HEIGHT-50,'created');
+        this.created.setDepth(999);
+        this.created.setScale(this.scale+0.3);
+
+
     
     }
 
     update(){
         super.update();
+    }
+
+    redirectToWeb() {
+        // Redirige a una página web
+        window.open('https://github.com/tysonmdg/CodeChronicles.github.io   ', '_blank');
     }
 }
     
